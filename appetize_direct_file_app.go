00223610fc56d230358ddfbe674d4cb856996706
Dima Golomozy <dima.golomozy@gmail.com>
2020-06-01 09:37:56 +0300

    split to url and file_path resource

---
+++ appetize_direct_file_app.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceAppetizeDirectFileApp() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAppetizeDirectFileAppCreate,
+		Read:   resourceAppetizeAppRead,
+		Delete: resourceAppetizeAppDelete,
+
+		Schema: func() map[string]*schema.Schema {
+			s := resourceAppetizeApp().Schema
+
+			delete(s, "url")
+			s["file_path"] = &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			}
+
+			// Everything is ForceNew
+			for _, v := range s {
+				v.ForceNew = true
+			}
+
+			return s
+		}(),
+	}
+}
+
+func resourceAppetizeDirectFileAppCreate(d *schema.ResourceData, m interface{}) error {
+	appetizer := NewAppetizer(d)
+	app, err := appetizer.CreateApp(NewAppOptions(d))
+	if err != nil {
+		//return fmt.Errorf("Error launching source instance: %s", err)
+		return err
+	}
+
+	if app == nil {
+		return fmt.Errorf("[ERROR] launching source instance: %s", err)
+	}
+
+	d.SetId(app.PublicKey)
+	return resourceAppetizeAppRead(d, m)
+}
--- provider.go
+++ provider.go
@@ -7,7 +7,8 @@ import (
 func Provider() *schema.Provider {
 	return &schema.Provider{
 		ResourcesMap: map[string]*schema.Resource{
-			"appetize_app": resourceApp(),
+			"appetize_app":             resourceAppetizeApp(),
+			"appetize_direct_file_app": resourceAppetizeDirectFileApp(),
 		},
 	}
 }
