87796c2d6d1182904c24113a5a3baffad60d8677
dimagolomozy <dima.golomozy@gmail.com>
2022-08-03 10:43:47 +0300

    update go and provider project structure

--- appetize/appetize_app.go
+++ appetize/appetize_app.go
@@ -1,9 +1,9 @@
-package main
+package appetize
 
 import (
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 )
 
 func resourceAppetizeApp() *schema.Resource {
